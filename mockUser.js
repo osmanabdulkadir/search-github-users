5d6335097ef633e71d45dadb9aa918901cb0929c
osman abdul <74805866+osmanabdulkadir@users.noreply.github.com>
2021-11-08 13:44:48 -0500

    Add files via upload

---
+++ mockUser.js
@@ -0,0 +1,38 @@
+export default {
+  login: "osmanabdulkadir",
+  id: 74805866,
+  node_id: "MDQ6VXNlcjc0ODA1ODY2",
+  avatar_url: "https://avatars.githubusercontent.com/u/74805866?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/osmanabdulkadir",
+  html_url: "https://github.com/osmanabdulkadir",
+  followers_url: "https://api.github.com/users/osmanabdulkadir/followers",
+  following_url:
+    "https://api.github.com/users/osmanabdulkadir/following{/other_user}",
+  gists_url: "https://api.github.com/users/osmanabdulkadir/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/osmanabdulkadir/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/osmanabdulkadir/subscriptions",
+  organizations_url: "https://api.github.com/users/osmanabdulkadir/orgs",
+  repos_url: "https://api.github.com/users/osmanabdulkadir/repos",
+  events_url: "https://api.github.com/users/osmanabdulkadir/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/osmanabdulkadir/received_events",
+  type: "User",
+  site_admin: false,
+  name: "osman abdul",
+  company: null,
+  blog: "",
+  location: "new york, new york",
+  email: null,
+  hireable: null,
+  bio: "front end web developer",
+  twitter_username: null,
+  public_repos: 61,
+  public_gists: 1,
+  followers: 0,
+  following: 1,
+  created_at: "2020-11-21T04:05:56Z",
+  updated_at: "2021-08-07T20:12:45Z"
+};
